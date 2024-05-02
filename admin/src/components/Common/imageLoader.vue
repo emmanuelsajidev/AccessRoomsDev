70b9dd6ff12068bb60176c25d85f1f7039273e1c
emmaunel <emmaunelsajidev@gmail.com>
2024-05-02 19:27:10 +0530

    small updates

--- admin/src/components/Common/imageLoader.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <v-row class="fill-height ma-0" align="center" justify="center">
-      <v-progress-circular indeterminate color="#ff6200"></v-progress-circular>
-    </v-row>
-  </template>
