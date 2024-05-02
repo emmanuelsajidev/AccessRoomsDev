749f562f0cf6a744ac9fafdf8d8706cb7683cecf
emmaunel <emmaunelsajidev@gmail.com>
2024-05-02 19:23:09 +0530

    major updates

--- agent/src/components/Common/test.vue
+++
@@ -1,6 +0,0 @@
-combineDateTime(dateString, timeString) {
-  const formattedDate = `${dateString}T${timeString}:00`;
-  return formattedDate;
-},
-startTime: this.combineDateTime(this.checkInDate, this.checkInTime),
-endTime: this.combineDateTime(this.checkInDate, this.checkOutTime),
